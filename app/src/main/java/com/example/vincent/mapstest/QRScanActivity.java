2c9d07e7e2a0ef9420ab6836b240ec6d1dd89894
VincentVanWijk <vincentvanwijk@hotmail.nl>
2016-09-21 11:15:14 +0200

    scanning a qr code opens a webview that shows the url

--- app/src/main/java/com/example/vincent/mapstest/QRScanActivity.java
+++ app/src/main/java/com/example/vincent/mapstest/QRScanActivity.java
@@ -1,11 +1,13 @@
 package com.example.vincent.mapstest;
 
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.google.zxing.BarcodeFormat;
 import com.google.zxing.Result;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
@@ -50,13 +52,16 @@ public class QRScanActivity extends AppCompatActivity implements ZXingScannerVie
     {
         Log.e("handler", result.getText());
         Log.e("handler", result.getBarcodeFormat().toString());
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-        builder.setTitle("Scan Result");
-        builder.setMessage(result.getText());
-        AlertDialog alert1 = builder.create();
+//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//        builder.setTitle("Scan Result");
+//        builder.setMessage(result.getText());
+//        AlertDialog alert1 = builder.create();
+//        alert1.show();
+//        mScannerView.resumeCameraPreview(this);
 
-        alert1.show();
-        mScannerView.resumeCameraPreview(this);
+        Intent i = new Intent(getApplicationContext(),WebViewActivity.class);
+        i.putExtra("url", result.getText());
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/vincent/mapstest/WebViewActivity.java
@@ -0,0 +1,30 @@
+package com.example.vincent.mapstest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewActivity extends AppCompatActivity
+{
+    String url;
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        webView = (WebView)findViewById(R.id.qrWebView);
+        webView.setWebViewClient(new WebViewClient());
+
+       Bundle extras = getIntent().getExtras();
+
+        if (extras != null)
+        {
+            url = extras.getString("url");
+            webView.loadUrl(url);
+        }
+
+    }
+}
