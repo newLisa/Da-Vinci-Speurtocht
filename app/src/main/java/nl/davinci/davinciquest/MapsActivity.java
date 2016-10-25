e409b91e764a02ef4e361305ed8396aac143ade3
VincentVanWijk <vincentvanwijk@hotmail.nl>
2016-10-25 13:06:24 +0200

    the map now shows a circle around the current location, the map now zooms to currentlocaton automaticly after 3 seconds

--- app/src/main/java/nl/davinci/davinciquest/MapsActivity.java
+++ app/src/main/java/nl/davinci/davinciquest/MapsActivity.java
@@ -3,8 +3,10 @@ package nl.davinci.davinciquest;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.os.AsyncTask;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
@@ -16,10 +18,14 @@ import android.widget.Button;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -48,12 +54,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        //ask for location permission
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
 
-        ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                1);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -65,6 +71,53 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         mapFragment.getMapAsync(this);
 
+        AddButtonOnClickListeners();
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap)
+    {
+        mMap = googleMap;
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED)
+        {
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
+    @Override
+    public  void onConnected(Bundle connectionHint)
+    {
+        //draw the circle around the current location
+        LatLng currentLocation = GetCurrentLocation();
+        Circle circle = mMap.addCircle(new CircleOptions()
+                .center(new LatLng(currentLocation.latitude, currentLocation.longitude))
+                .radius(500)
+                .strokeColor(Color.RED)
+                .fillColor(Color.TRANSPARENT));
+
+        //use Handler to start zoom function after 3 seconds
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ZoomCameraToCurrentPosition();
+            }
+        }, 3000);
+    }
+
+    public void AddButtonOnClickListeners()
+    {
         markerButton = (Button) findViewById(R.id.currentLocMarkerButton);
         markerButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,6 +126,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 SetMarkerAtCurrentLocation();
             }
         });
+
         qrButton = (FloatingActionButton) findViewById(R.id.floatingQRbutton);
         qrButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,7 +137,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
-
         speurtochtButton = (Button) findViewById(R.id.getSpeurtochButton);
         speurtochtButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -92,10 +145,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 GetSpeurtocht();
             }
         });
-
-
     }
 
+    public void ZoomCameraToCurrentPosition()
+    {
+        LatLng currentPos = GetCurrentLocation();
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(currentPos)      // Sets the center of the map to location user
+                .zoom(16)                   // Sets the zoom
+                .bearing(0)                // Sets the orientation of the camera to north
+                .tilt(30)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
 
     public void GetSpeurtocht()
     {
@@ -140,33 +202,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap)
+    public LatLng GetCurrentLocation()
     {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        //LatLng sydney = new LatLng(-34, 151);
-        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-
+        LatLng pos = null;
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED)
         {
-            mMap.setMyLocationEnabled(true);
-        } else
-        {
-            // Show rationale and request permission.
+            Location currentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            pos = new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude());
         }
+        return pos;
     }
 
     protected void onStart()
@@ -179,12 +224,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     {
         mGoogleApiClient.disconnect();
         super.onStop();
-    }
-
-    @Override
-    public  void onConnected(Bundle connectionHint)
-    {
-
     }
 
     @Override
